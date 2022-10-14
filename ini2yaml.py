b31da745b7c5e84843de705f54ac4b40ea130309
Lee Johnson <lee.james.johnson@gmail.com>
2022-10-14 18:01:29 -0400

    master - 	modified:   ini2yaml.py

--- ini2yaml.py
+++ ini2yaml.py
@@ -24,7 +24,7 @@ yaml.add_representer(literal_unicode, literal_unicode_representer)
 config = ConfigParser.RawConfigParser(allow_no_value = True)
 config.optionxform = str
 # config.readfp(sys.stdin)
-config_reader = config.parser.read_file
+config_reader = config.read_file
 config_reader(sys.stdin)
 
 inventory = {}
