68058d4c3816c994bf5e32c4ebac251670b3a54d
Lee Johnson <lee.james.johnson@gmail.com>
2022-10-14 18:05:47 -0400

    master - 	modified:   ini2yaml.py

--- ini2yaml.py
+++ ini2yaml.py
@@ -68,6 +68,8 @@ for section in config.sections():
       inventory.setdefault('all', {}).setdefault('children', {}).setdefault(group[0], {}).setdefault('hosts', {})[hostname] = {}
       for hostvar in hostvars:
         value = parse_value(hostvar[1])
+        print("hostvar => %s", hostvar)
+        print("value => %s", value)
         inventory.setdefault('all', {}).setdefault('children', {}).setdefault(group[0], {}).setdefault('hosts', {})[hostname][hostvar[0]] = value
   elif group[1] == 'vars':  # section contains group vars
     for name, value in config.items(section):
