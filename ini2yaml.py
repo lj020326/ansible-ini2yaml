fd8064cbb4468dd6da89c36a726b6044c7bc71d9
Lee Johnson <lee.james.johnson@gmail.com>
2022-10-14 18:07:01 -0400

    master - 	modified:   ini2yaml.py 	modified:   inventory.yml

--- ini2yaml.py
+++ ini2yaml.py
@@ -68,6 +68,7 @@ for section in config.sections():
       inventory.setdefault('all', {}).setdefault('children', {}).setdefault(group[0], {}).setdefault('hosts', {})[hostname] = {}
       for hostvar in hostvars:
         print("hostvar => %s", hostvar)
+        print("hostvar[1] => %s", hostvar[1])
         value = parse_value(hostvar[1])
         print("value => %s", value)
         inventory.setdefault('all', {}).setdefault('children', {}).setdefault(group[0], {}).setdefault('hosts', {})[hostname][hostvar[0]] = value
